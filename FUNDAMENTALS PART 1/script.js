b9002137df711e90b526ca4daee4049d8aac68dd
Amer Shaqra <eyepoker83@gmail.com>
2022-06-19 14:55:00 +0300

    hey

--- FUNDAMENTALS PART 1/script.js
+++ FUNDAMENTALS PART 1/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function Book(tittle, author, numberOfPages, didYouRead) {
+  this.tittle = tittle;
+  this.author = author;
+  this.numberOfPages = null;
+  this.didYouRead = didYouRead;
+  this.tellInfo = function () {
+    let info = `${tittle} by ${author}, ${numberOfPages}, ${didYouread}`;
+    return info;
+  };
+}
